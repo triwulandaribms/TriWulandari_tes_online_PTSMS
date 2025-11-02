fc35dd0f95c0810d71298d6d323b8ec6e3d33a37
Tri Wulandari <triwulandari3456@gmail.com>
2025-11-02 11:37:48 +0700

    update api dokumentasi

--- routes/api.php
+++ routes/api.php
@@ -8,22 +8,19 @@ use App\Http\Controllers\PembelianController;
 Route::post('/register', [UserController::class, 'registrasi']); 
 Route::post('/login', [UserController::class, 'login']);       
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'check.sanctum.token'])->group(function () {
 
-    // USER
     Route::get('/list-user', [UserController::class, 'listUser']);       
     Route::put('/update-user-by/{id}', [UserController::class, 'update']);
     Route::delete('/delete-user-by/{id}', [UserController::class, 'hapus']); 
     Route::post('/logout', [UserController::class, 'logout']);     
 
-    // BARANG
     Route::get('/barang', [BarangController::class, 'tampilAll']);
     Route::post('/barang', [BarangController::class, 'tambah']);
     Route::get('/barang/{id}', [BarangController::class, 'tampil']);
     Route::put('/barang/{id}', [BarangController::class, 'update']);
     Route::delete('/barang/{id}', [BarangController::class, 'hapus']);
 
-    // PEMBELIAN
     Route::get('/pembelian', [PembelianController::class, 'tampilAll']);
     Route::get('/pembelian/{id}', [PembelianController::class, 'tampil']);
     Route::post('/pembelian', [PembelianController::class, 'tambah']);
@@ -31,3 +28,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/pembelian/{id}', [PembelianController::class, 'hapus']);
     Route::get('/report/pembelian', [PembelianController::class, 'report']);
 });
+
