1e8fd28c530ad2213e46adc4ecec8648e3b30802
Tri Wulandari <triwulandari3456@gmail.com>
2025-11-12 19:02:22 +0700

    coba vite

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title', 'Laravel AJAX App')</title>
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  @vite(['resources/css/style.css', 'resources/js/app.js'])
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 <body>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', fn() => view('users.login'));
-Route::get('/register', fn() => view('users.register'));
+Route::get('/login', fn() => view('user.login'));
+Route::get('/register', fn() => view('user.register'));
 Route::get('/barang', fn() => view('barang.index'));
 Route::get('/barang/form', fn() => view('barang.form'));
 Route::get('/pembelian', fn() => view('pembelian.index'));
